c2e71fc04a12150af9d1c71a11edbd9c89a7c532
Guillaume Macret <guillaume.macret@gmail.com>
2020-03-27 11:41:56 +0100

    :tada: Unity client

---
+++ ClientUnity/Assets/Scripts/UnityAndroidTuto/MultiTouchDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiTouchDisplay : MonoBehaviour
+{
+    public Text multiTouchInfoDisplay;
+    private int maxTapCount = 0;
+    private string multiTouchInfo;
+    private Touch theTouch;
+
+    // Update is called once per frame
+    void Update()
+    {
+        multiTouchInfo = string.Format("Max tap count: {0}\n", maxTapCount);
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                theTouch = Input.GetTouch(i);
+                multiTouchInfo += string.Format("Touch {0} - Position {1} - Tap count: {2} - Finger ID : {3}\n" +
+                    "Radius {4} ({5}%)\n"
+                    ,i,theTouch.position,theTouch.tapCount,theTouch.fingerId,theTouch.radius, ((theTouch.radius / (theTouch.radius + theTouch.radiusVariance)) * 100f).ToString("F1"));
+                if(theTouch.tapCount > maxTapCount)
+                {
+                    maxTapCount = theTouch.tapCount;
+                }
+            }
+        }
+        multiTouchInfoDisplay.text = multiTouchInfo;
+    }
+}
---
+++ ClientUnity/Assets/Scripts/UnityAndroidTuto/TouchPhaseDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchPhaseDisplay : MonoBehaviour
+{
+    public Text phaseDisplayText;
+    private Touch theTouch;
+    private float timeTouchEnded;
+    private float displayTime = .5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            theTouch = Input.GetTouch(0);
+            phaseDisplayText.text = theTouch.phase.ToString();
+
+            if(theTouch.phase == TouchPhase.Ended)
+            {
+                timeTouchEnded = Time.time;
+            }
+        }else if(Time.time - timeTouchEnded > displayTime)
+        {
+            phaseDisplayText.text = "";
+        }
+    }
+}

7c8c845f134e6f54d3af71c7774047758c281737
QuentinSauvage <sauvage.quentin62@gmail.com>
2020-04-19 16:10:20 +0200

    :sparkles: New map

---
+++ Tiled/TiledAssets/Enemy 05-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="Enemy 05-1" tilewidth="64" tileheight="64" tilecount="12" columns="3">
+ <image source="../PIPOYA FREE RPG Character Sprites 32x32/Enemy/Enemy 05-1.png" trans="ff00ff" width="192" height="256"/>
+</tileset>

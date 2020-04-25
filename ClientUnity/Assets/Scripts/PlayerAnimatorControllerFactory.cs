6fa642386a55e6c515429d813372b6539bcafa30
Guillaume Macret <guillaume.macret@gmail.com>
2020-04-25 10:38:25 +0200

    :sparkles: new animations for players

--- ClientUnity/Assets/Scripts/PlayerAnimatorControllerFactory.cs
+++ ClientUnity/Assets/Scripts/PlayerAnimatorControllerFactory.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 
 public class PlayerAnimatorControllerFactory : MonoBehaviour
 {
+    public const int ROLE_PHANTOM = 1;
+    public const int ROLE_KNIGHT = 2;
+    public const int ROLE_WAR = 3;
+    public const int ROLE_PUMKIN = 4;
+    public const int ROLE_MAGE = 5;
+    public const int ROLE_ELF = 6;
+
+    public readonly string[] roleAnimationPrefixes = {"phantom","knight","war","pumkin","mage","elf" };
+
     public List<AnimatorController> playerAnimators;
+
     public AnimatorController GetAnimatorController(int id)
     {
         if (id > playerAnimators.Count - 1)

ebe8ef0b936e229f62db6bb40b329362a98353fc
Kairozen <geoffbaudelet@yahoo.fr>
2020-04-26 02:11:32 +0200

    :sparkles: GameList (still needs to be tested with server)

---
+++ ClientUnity/Assets/Scripts/GameListDetailsScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameListDetailsScript : MonoBehaviour
+{
+	public Text GameIDText;
+	public int NbPlayers = 0;
+	public Button JoinButton;
+
+	public GameListDetailsScript(string gameID, int nbPlayers) {
+		GameIDText.text = gameID;
+		NbPlayers = nbPlayers;
+	}
+}

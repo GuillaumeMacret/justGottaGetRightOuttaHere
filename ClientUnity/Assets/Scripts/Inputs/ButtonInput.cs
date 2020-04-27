e6fe5c6956bb64f2b57950a8402c83a9f697abf8
Guillaume Macret <guillaume.macret@gmail.com>
2020-04-27 11:54:49 +0200

    :sparkles: added "arrow"

---
+++ ClientUnity/Assets/Scripts/Inputs/ButtonInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour
+{
+    public void SendMoveMessage(string direction)
+    {
+        string messageToSend = MessageBuilders.BuildMovementMessage(direction);
+        TCPClient.SendMessage(messageToSend);
+    }
+
+    public void SendActionMessage()
+    {
+        string messageToSend = MessageBuilders.BuildActionMessage();
+        TCPClient.SendMessage(messageToSend);
+    }
+}

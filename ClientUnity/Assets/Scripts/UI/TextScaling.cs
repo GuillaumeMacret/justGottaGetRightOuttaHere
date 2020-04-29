db0a79b0dcf40f81744584989307fe2ea00c0b68
Guillaume Macret <guillaume.macret@gmail.com>
2020-04-29 12:42:01 +0200

    :sparkles: Using canvas scaler

---
+++ ClientUnity/Assets/Scripts/UI/TextScaling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScaling : MonoBehaviour
+{
+    public int size;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<Text>().fontSize = Screen.width / size;
+    }
+    private void Update()
+    {
+        GetComponent<Text>().fontSize =(int) (Screen.width / (float) size);
+    }
+}
